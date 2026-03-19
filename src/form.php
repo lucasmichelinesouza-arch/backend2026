de538369131803968df574862781fd2278e193c1
lucasmichelinesouza-arch <lucasmichelinesouza@gmail.com>
2026-03-19 00:29:50 +0000

    aula 18/03

---
+++ src/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title> <link rel="stylesheet" href="form.css">
+</head>
+    <body>
+    <h1>Adega UniNine</h1>
+    <form method="post" action="recebe.php">
+    Login<input type="text" id="login" name="login"> <br> <br>
+    Senha<input type="password" id="senha" name="senha"> <br> <br>
+    <input type="submit" value="Entrar">
+    </form>
+    </body>
+</html>
